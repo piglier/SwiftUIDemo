58a516daba7b5d93c0a6a3e77a6e3a91befd8cc2
JasonZhu <JasonZhu@taiwanmobile.com>
2024-02-26 10:15:13 +0800

    [add] layout version two

--- SwiftUIDemo/SwiftUIDemo/ContentView.swift
+++ SwiftUIDemo/SwiftUIDemo/ContentView.swift
@@ -8,75 +8,126 @@
 import SwiftUI
 import CoreData
 
+
 struct ContentView: View {
-    let foods = Food.examples
     @State var selectedFood: Food?
+    @State var shouldShowInfo: Bool = false
+    
+    let foods = Food.examples
     
     var body: some View {
-        VStack(spacing: 30) {
-            Group {
-                if (selectedFood == .none) {
-                    Image("dinner")
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                } else {
-                    Text(selectedFood!.image)
-                        .font(.system(size: 200))
-                        .minimumScaleFactor(0.5)
-                        .lineLimit(1)
+        ScrollView {
+            VStack(spacing: 30) {
+                Group {
+                    if (selectedFood == .none) {
+                        Image("dinner")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                    } else {
+                        Text(selectedFood!.image)
+                            .font(.system(size: 200))
+                            .minimumScaleFactor(0.5)
+                            .lineLimit(1)
+                    }
                 }
-            }
-            .frame(height: 250)
-            Text("今天吃什麼?")
-                .bold()
-            Spacer()
-            if let food = selectedFood {
-                Text(food.name)
-                    .font(.largeTitle)
+                .frame(height: 250)
+                Text("今天吃什麼?")
                     .bold()
-                    .foregroundStyle(.green)
-                    .id(selectedFood?.name)
-                    .transition(.asymmetric(
-                        insertion: .opacity.animation(.easeInOut(duration: 0.5).delay(0.2)),
-                        removal: .opacity.animation(.easeInOut(duration: 0.4))
-                    ))
-                    
+                
+                if let food = selectedFood {
+                    VStack {
+                        HStack {
+                            Text(food.name)
+                                .font(.largeTitle)
+                                .bold()
+                                .foregroundStyle(.green)
+                                .id(food.name)
+                                .transition(.asymmetric(
+                                    insertion: .opacity.animation(.easeInOut(duration: 0.5).delay(0.2)),
+                                    removal: .opacity.animation(.easeInOut(duration: 0.4))
+                                ))
+                            Button(action: {
+                                shouldShowInfo.toggle()
+                            }) {
+                                Image(systemName: "info.circle.fill")
+                                    .foregroundStyle(.secondary)
+                            }
+                            .buttonStyle(.plain)
+                        }
+                        Text("熱量 \(food.calorie.formatted()) 大卡")
+                            .font(.title2)
+                        VStack {
+                            if (shouldShowInfo) {
+                                Grid(horizontalSpacing: 40, verticalSpacing: 12) {
+                                    GridRow{
+                                        Text("蛋白質")
+                                        Text("脂肪")
+                                        Text("碳水")
+                                    }.frame(minWidth: 60)
+                                    Divider()
+                                        .gridCellUnsizedAxes(.horizontal)
+                                        .padding(.horizontal, -10)
+                                    GridRow{
+                                        Text(food.protein.formatted())
+                                        Text(food.fat.formatted() + " g")
+                                        Text(food.$carb)
+                                    }
+                                }
+                                .font(.title3)
+                                .padding(.horizontal)
+                                .padding()
+                                .background(RoundedRectangle(cornerRadius: 8).foregroundStyle(Color(.systemBackground)))
+                                .transition(.move(edge: .top).combined(with: .opacity))
+                            }
+                        }.frame(maxWidth: .infinity).clipped()
+                        
+                    }
+                }
+                Spacer().layoutPriority(1.0)
+                Button() {
+                    selectedFood = foods.shuffled().filter { $0 != selectedFood }.first
+                } label: {
+                    Text(selectedFood == nil ? "告訴我" : "換一個")
+                        .frame(width: 200)
+                        .transformEffect(.identity)
+                }
+                .padding(.bottom, -15)
+                
+                Button() {
+                    selectedFood = .none
+                    shouldShowInfo = false
+                } label: {
+                    Text("重置")
+                        .frame(width: 200)
+                }
+                .buttonStyle(.bordered)
             }
-            Button() {
-                selectedFood = foods.shuffled().filter { $0 != selectedFood }.first
-            } label: {
-                Text(selectedFood == nil ? "告訴我" : "換一個")
-                    .frame(width: 200)
-                    .transformEffect(.identity)
-            }
-            .padding(.bottom, -15)
-            
-            Button() {
-                selectedFood = .none
-            } label: {
-                Text("重置")
-                    .frame(width: 200)
-            }
-            .buttonStyle(.bordered)
-        }
-        .padding()
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(Color(.secondarySystemBackground))
-        .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
-        .buttonStyle(.borderedProminent)
-        .buttonBorderShape(.capsule)
-        .controlSize(.large)
-        .animation(.easeInOut(duration: 0.5), value: selectedFood)
+            .padding()
+            .frame(maxWidth: .infinity, minHeight: UIScreen.main.bounds.height - 100)
+            .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+            .buttonStyle(.borderedProminent)
+            .buttonBorderShape(.capsule)
+            .controlSize(.large)
+            .animation(.easeInOut(duration: 0.5), value: selectedFood)
+            .animation(.spring(dampingFraction: 0.5), value: shouldShowInfo)
+        }.background(Color(.secondarySystemBackground))
     }
+    
     
 }
 
-//#Preview {
-//    ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
-//}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(selectedFood: Food.examples.first!)
+        ContentView(selectedFood: Food.examples.first!).previewDevice(.iPad)
+        ContentView(selectedFood: Food.examples.first!).previewDevice(.iPhoneSE)
+    }
+}
+
+
+extension ContentView {
+    init(selectedFood: Food) {
+        _selectedFood = .init(wrappedValue: selectedFood)
     }
 }
---
+++ SwiftUIDemo/SwiftUIDemo/PreviewDevice.swift
@@ -0,0 +1,13 @@
+//
+//  PreviewDevice.swift
+//  SwiftUIDemo
+//
+//  Created by 朱彥睿 on 2024/2/24.
+//
+
+import SwiftUI
+
+extension PreviewDevice {
+    static let iPad = PreviewDevice(rawValue: "iPad Pro (12.9-inch) (6th generation)")
+    static let iPhoneSE = PreviewDevice(rawValue: "iPhone SE (3rd generation)")
+}
